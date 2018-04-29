c1aee3eaee0c3db58fc9fb8a1c071a8f45e9566c
Mahdi89 <jelodari.m@gmail.com>
2018-04-29 22:30:02 +0100

    add a parser

--- src/parser.go
+++ src/parser.go
@@ -5,11 +5,11 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"os"
 	s "strings"
 )
-
 
 func check(e error) {
 	if e != nil {
@@ -17,25 +17,28 @@ func check(e error) {
 	}
 }
 
-func main() {
+type Node struct {
+	name string
+	next *Node
+}
 
-	f, err := os.Open("./callgraph.dot")
+type Node_ struct {
+	name string
+	id   string
+}
+
+func Parse(file string) (*list.List, *list.List)  {
+
+	f, err := os.Open(file)
 	check(err)
 	defer f.Close()
 
-	scanstd := bufio.NewScanner(os.Stdin)
-
-	// Get the src and dist nodes
-	scanstd.Scan()
-	caller := scanstd.Text()
-	scanstd.Scan()
-	callee := scanstd.Text()
-
-	fmt.Println(caller, callee)
-
-	// Parse the generated dot graph 
+	// Parse the generated dot graph and return a list
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanWords)
+
+	l := list.New()
+	l_ := list.New()
 
 	for scanner.Scan() {
 		str := scanner.Text()
@@ -45,8 +48,44 @@ func main() {
 			if s.HasPrefix(nxt, "->") {
 				scanner.Scan()
 				str2 := scanner.Text()
-				fmt.Println(str2)
+				e1 := Node{str2, nil}
+				e2 := Node{str, &e1}
+				l.PushBack(e2)
+
+			}else {
+				e1 := Node_{nxt, str}
+				l_.PushBack(e1)
 			}
 		}
 	}
+	return l,l_
+}
+
+
+func main() {
+
+	file := "./callgraph.dot"
+
+	// Parse the input file
+	l,l_ := Parse(file)
+
+	// Get Caller and Callee nodes
+	scanstd := bufio.NewScanner(os.Stdin)
+	scanstd.Scan()
+	caller := scanstd.Text()
+	scanstd.Scan()
+	callee := scanstd.Text()
+
+	fmt.Println(caller, callee)
+
+	for e := l_.Front(); e != nil; e = e.Next() {
+		// do something with e.Value
+		fmt.Println(e.Value.(Node_).name)
+	}
+
+	for e := l.Front(); e != nil; e = e.Next() {
+		// do something with e.Value
+		fmt.Println(e.Value.(Node).name)
+	}
+
 }
